16e93653c0f6a89cbaa3e9447a065d7ae0bfc7f2
kuntoaji <kunto.aji.kr@gmail.com>
2011-09-04 13:01:05 +0700

    update rails version 3.0.9

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.7'
+gem 'rails', '3.0.9'
 gem 'sqlite3-ruby', :require => 'sqlite3'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MyApp::Application.config.secret_token = 'eabd7f1efc9ae5097c2b1c40609279d0ebe54db83cca5b4d32776d982265e2b38146915bde9ad3bf377b225d3fc341c2b014fa802267fcf8cba7b1caa86a6e7c'
+MyApp::Application.config.secret_token = '63963dbc40157a94cd1ccf18f6daa670bc1efdcc92ce92e3af623aaebfc5e3977d8c44486a37f11c57d72d2b493faf23e174b9ffac3e708006282a9a11c124df'
